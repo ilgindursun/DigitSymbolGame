f9f1fc85026f72f9a58fbd7ed177c1c137833a1a
Ilgın Dursun <ilgindursun@192.168.1.11>
2026-03-26 18:02:38 +0300

    Reapply "Add DigitSymbolGame component"
    This reverts commit e4d20d56d1834c52cd33565af84ecc4a1beabc95.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import DigitSymbolGame from './DigitSymbolGame.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <DigitSymbolGame />
   </StrictMode>,
 )
